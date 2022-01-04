dc7968808c941947395051ae9e40896e9497413c
Tomasz <massamo24@gmail.com>
2022-01-04 12:13:52 +0100

    Basic commit

---
+++ SpringForumApp/src/test/java/pl/tostrowski/SpringForumApp/SpringForumAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tostrowski.SpringForumApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringForumAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
